6f60401da95a820b3f151a6d5b54c1f30c0fecaf
1yangsh <ysh410@gmail.com>
2021-01-20 17:50:20 +0900

    url_view_template update

--- mybookmark/bookmarkapp/views.py
+++ mybookmark/bookmarkapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from bookmarkapp.models import Bookmark
+
+
+class BookmarkLV(ListView):
+    # url + "_list.html"
+    model = Bookmark
+
+
+class BookmarkDV(DetailView):
+    # url + "_detail.html"
+    model = Bookmark
